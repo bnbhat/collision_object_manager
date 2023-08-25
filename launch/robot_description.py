4828741aa582e6328661d4a0b2737b5a5830d02f
balachandra <bhalu311@gmail.com>
2023-08-25 12:16:59 +0200

    update launch file

---
+++ launch/robot_description.py
@@ -0,0 +1,83 @@
+from launch.substitutions import PathJoinSubstitution, Command, FindExecutable
+from launch_ros.substitutions import FindPackageShare
+
+def get_robot_description():
+    joint_limit_params = PathJoinSubstitution(
+        [FindPackageShare("ur_description"), "config", "ur5e", "joint_limits.yaml"]
+    )
+    kinematics_params = PathJoinSubstitution(
+        [FindPackageShare("ur_description"), "config", "ur5e", "default_kinematics.yaml"]
+    )
+    physical_params = PathJoinSubstitution(
+        [FindPackageShare("ur_description"), "config", "ur5e", "physical_parameters.yaml"]
+    )
+    visual_params = PathJoinSubstitution(
+        [FindPackageShare("ur_description"), "config", "ur5e", "visual_parameters.yaml"]
+    )
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution([FindPackageShare("ur_description"), "urdf", "ur.urdf.xacro"]),
+            " ",
+            "robot_ip:=192.168.56.101",
+            " ",
+            "joint_limit_params:=",
+            joint_limit_params,
+            " ",
+            "kinematics_params:=",
+            kinematics_params,
+            " ",
+            "physical_params:=",
+            physical_params,
+            " ",
+            "visual_params:=",
+            visual_params,
+            " ",
+           "safety_limits:=",
+            "true",
+            " ",
+            "safety_pos_margin:=",
+            "0.15",
+            " ",
+            "safety_k_position:=",
+            "20",
+            " ",
+            "name:=",
+            "ur",
+            " ",
+            "ur_type:=",
+            "ur5e",
+            " ",
+            "prefix:=",
+            '""',
+            " ",
+        ]
+    )
+
+
+    robot_description = {"robot_description": robot_description_content}
+    return robot_description
+
+def get_robot_description_semantic():
+    # MoveIt Configuration
+    robot_description_semantic_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution([FindPackageShare("ur_moveit_config"), "srdf", "ur.srdf.xacro"]),
+            " ",
+            "name:=",
+            # Also ur_type parameter could be used but then the planning group names in yaml
+            # configs has to be updated!
+            "ur",
+            " ",
+            "prefix:=",
+            '""',
+            " ",
+        ]
+    )
+    robot_description_semantic = {
+        "robot_description_semantic": robot_description_semantic_content
+    }
+    return robot_description_semantic
