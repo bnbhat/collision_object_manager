09468c6a75ba4572266c3137e22ff19e744ece44
balachandra <bhalu311@gmail.com>
2023-09-07 08:30:06 +0200

    add a wrapper class for human skeleton

---
+++ include/collision_object_manager/human_skeleton.hpp
@@ -0,0 +1,56 @@
+/**
+ * @file human_skeleton.hpp
+ * @author Balachandra Bhat (bnbhat311@gmail.com)
+ * @brief contains human skeleton related functions
+ * @version 1.0
+ * @date 2023-09-05
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#pragma once
+
+#include <vector>
+#include "collision_object_manager/custom_data_objects.hpp"
+
+class Skeleton {
+    public:
+        Skeleton(const arc_interfaces::msg::ArcHumanPose& pose);
+        //virtual std::vector<Point3d> get_arm_points() = 0;
+        virtual std::vector<Point3d> get_right_arm_points() = 0;
+        virtual std::vector<Point3d> get_left_arm_points() = 0;
+    
+    protected:
+        PoseMsg m_pose;
+        template <typename EnumType>
+        Point3d get_point_of(EnumType enum_value);
+};
+
+class Body18 : public Skeleton {
+    public:
+        Body18(const ArcHumanPose& pose);
+        std::vector<Point3d> get_right_arm_points() override;
+        std::vector<Point3d> get_left_arm_points() override;
+};
+
+class Body34 : public Skeleton {
+    public:
+        Body34(const ArcHumanPose& pose);
+        std::vector<Point3d> get_right_arm_points() override;
+        std::vector<Point3d> get_left_arm_points() override;
+};
+
+class Body38 : public Skeleton {
+    public:
+        Body38(const ArcHumanPose& pose);
+        std::vector<Point3d> get_right_arm_points() override;
+        std::vector<Point3d> get_left_arm_points() override;
+};
+
+class SkeletonFactory {
+    public:
+        static std::unique_ptr<Skeleton> get_skeleton(const ArcHumanPose& pose);
+};
+
+
---
+++ src/human_skeleton.cpp
@@ -0,0 +1,97 @@
+#include "collision_object_manager/human_skeleton.hpp"
+
+
+std::unique_ptr<Skeleton> SkeletonFactory::get_skeleton(const ArcHumanPose &pose)
+{   
+    switch (static_cast<HUMAN_SKELETON_TYPE>(pose.type)) {
+            case HUMAN_SKELETON_TYPE::BODY_18:
+                return std::make_unique<Body18>(pose);
+            case HUMAN_SKELETON_TYPE::BODY_34:
+                return std::make_unique<Body34>(pose);
+            case HUMAN_SKELETON_TYPE::BODY_38:
+                return std::make_unique<Body38>(pose);
+            default:
+                throw std::invalid_argument("Invalid skeleton type");
+        }
+}
+
+Skeleton::Skeleton(const ArcHumanPose &pose)
+{   
+    m_pose = PoseMsg();
+    m_pose.key_points = pose.key_points;
+    m_pose.type = static_cast<HUMAN_SKELETON_TYPE>(pose.type);
+}
+
+template <typename EnumType>
+Point3d Skeleton::get_point_of(EnumType enum_value) {
+    int index = static_cast<int>(enum_value);
+
+    if (index >= 0 && index < static_cast<int>(m_pose.key_points.size())) {
+        return get_point_of(index);
+    } else {
+        throw std::out_of_range("Invalid enum value provided.");
+    }
+}
+
+Body18::Body18(const ArcHumanPose &pose)
+: Skeleton(pose)
+{
+}
+
+Body34::Body34(const ArcHumanPose &pose)
+: Skeleton(pose)
+{
+}
+
+Body38::Body38(const ArcHumanPose &pose)
+: Skeleton(pose)
+{
+}
+
+std::vector<Point3d> Body18::get_right_arm_points()
+{   
+    std::vector<Point3d> arm_points;
+    arm_points.emplace_back(get_point_of(BODY_18::RIGHT_ELBOW));
+    arm_points.emplace_back(get_point_of(BODY_18::RIGHT_WRIST));
+    return arm_points;
+}
+
+std::vector<Point3d> Body34::get_right_arm_points()
+{
+    std::vector<Point3d> arm_points;
+    arm_points.emplace_back(get_point_of(BODY_34::RIGHT_ELBOW));
+    arm_points.emplace_back(get_point_of(BODY_34::RIGHT_WRIST));
+    return arm_points;
+}
+
+std::vector<Point3d> Body38::get_right_arm_points()
+{
+    std::vector<Point3d> arm_points;
+    arm_points.emplace_back(get_point_of(BODY_38::RIGHT_ELBOW));
+    arm_points.emplace_back(get_point_of(BODY_38::RIGHT_WRIST));
+    return arm_points;
+}
+
+std::vector<Point3d> Body18::get_left_arm_points()
+{   
+    std::vector<Point3d> arm_points;
+    arm_points.emplace_back(get_point_of(BODY_18::LEFT_ELBOW));
+    arm_points.emplace_back(get_point_of(BODY_18::LEFT_WRIST));
+    return arm_points;
+}
+
+std::vector<Point3d> Body34::get_left_arm_points()
+{
+    std::vector<Point3d> arm_points;
+    arm_points.emplace_back(get_point_of(BODY_34::LEFT_ELBOW));
+    arm_points.emplace_back(get_point_of(BODY_34::LEFT_WRIST));
+    return arm_points;
+}
+
+std::vector<Point3d> Body38::get_left_arm_points()
+{
+    std::vector<Point3d> arm_points;
+    arm_points.emplace_back(get_point_of(BODY_38::LEFT_ELBOW));
+    arm_points.emplace_back(get_point_of(BODY_38::LEFT_WRIST));
+    return arm_points;
+}
