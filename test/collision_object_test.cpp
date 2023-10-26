9f3ad0d9f9729b853547a9e3d7c1711b3faefe86
balachandra <bhalu311@gmail.com>
2023-10-26 21:49:28 +0200

    update test case as per the workspace config

--- test/collision_object_test.cpp
+++ test/collision_object_test.cpp
@@ -26,14 +26,14 @@ int main(int argc, char** argv)
     shape_msgs::msg::SolidPrimitive primitive;
     primitive.type = primitive.BOX;
     primitive.dimensions.resize(3);
-    primitive.dimensions[primitive.BOX_X] = 0.5;
-    primitive.dimensions[primitive.BOX_Y] = 0.1;
-    primitive.dimensions[primitive.BOX_Z] = 0.5;
+    primitive.dimensions[primitive.BOX_X] = 0.1;
+    primitive.dimensions[primitive.BOX_Y] = 0.5;
+    primitive.dimensions[primitive.BOX_Z] = 0.3;
 
     geometry_msgs::msg::Pose box_pose;
     box_pose.orientation.w = 1.0;
-    box_pose.position.x = 0.8;
-    box_pose.position.y = 0.0;
+    box_pose.position.x = 0.0;
+    box_pose.position.y = 0.8;
     box_pose.position.z = 0.25;
 
     // Add collision object
@@ -53,8 +53,8 @@ int main(int argc, char** argv)
 
         if (path)
         {
-            box_pose.position.x += 0.01;
-            //box_pose.position.y += 0.01;
+            //box_pose.position.x += 0.01;
+            box_pose.position.y += 0.005;
             //box_pose.position.z += 0.01;
 //
             std::cout<< "x : " << box_pose.position.x << "  |  y : " << box_pose.position.y << "  |  z : " << box_pose.position.z << std::endl;
@@ -63,8 +63,8 @@ int main(int argc, char** argv)
         }
         else
         {
-            box_pose.position.x -= 0.01;
-            //box_pose.position.y -= 0.01;
+            //box_pose.position.x -= 0.01;
+            box_pose.position.y -= 0.005;
             //box_pose.position.z -= 0.01;
 
             std::cout<< "x : " << box_pose.position.x << "  |  y : " << box_pose.position.y << "  |  z : " << box_pose.position.z << std::endl;
@@ -72,14 +72,14 @@ int main(int argc, char** argv)
             collision_object_manager->moveRTCollisionObject(collision_object_poses);
         }
 
-        if(box_pose.position.x >= 0.8){
+        if(box_pose.position.y >= 0.8){
             path = false;
         }
-        else if(box_pose.position.x <= 0.5){
+        else if(box_pose.position.y <= 0.5){
             path = true;
         }
 
-        rclcpp::sleep_for(std::chrono::milliseconds(100));
+        rclcpp::sleep_for(std::chrono::milliseconds(50));
     }
 
     rclcpp::shutdown();
